38be2a34fd4290b8a9aff8848cb37e3a12396763
BasharSaad12 <47093295+BasharSaad12@users.noreply.github.com>
2019-02-11 09:46:33 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,4 +125,4 @@ if (message.content.startsWith(adminprefix + 'setava')) {
 });
 
 
-client.login("NTQwMTYyNTUwNzEzNTQ4ODEw.DzM5Pg.LbWV-Nr2XLvqXRkYMqbhmJ4_xwM");
+client.login("NTQ0MzQ1MzIzMjQyMjU4NDMz.D0Jzew.awhdm2TbJuGa18jGRvzjggNnNzs");
